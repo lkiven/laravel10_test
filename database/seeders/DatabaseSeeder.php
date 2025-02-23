f391d444f8ab8157f96d205cd28eca11d57dddf5
zz_lkw <zz_lkw@163.com>
2025-02-23 15:34:00 +0800

    修改：填充角色和管理员

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,6 +9,19 @@ class PermissionSeeder extends Seeder
 {
     public function run()
     {
+        // 创建权限
         Permission::create(['name' => 'admin']);
+
+        // 创建角色
+        $adminRole = Role::create(['name' => 'admin']);
+        $adminRole->givePermissionTo('admin');
+
+        // 创建管理员用户
+        $adminUser = User::factory()->create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('password')
+        ]);
+        $adminUser->assignRole('admin');
     }
 }
